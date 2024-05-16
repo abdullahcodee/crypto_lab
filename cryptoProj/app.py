0e19f21a417369a02549b478aad99e0f33dd865d
Abduallah El-Khodary <63024842+abdullahcodee@users.noreply.github.com>
2024-05-16 16:59:08 +0300

    last

--- cryptoProj/app.py
+++ cryptoProj/app.py
@@ -1,11 +1,10 @@
-from flask import Flask, redirect, render_template, request, url_for, flash,session
+from flask import Flask, redirect, render_template, request, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email
 from werkzeug.security import generate_password_hash, check_password_hash
-from pycipher import Caesar, Atbash, SimpleSubstitution, Affine, Autokey, Beaufort, PolybiusSquare, Playfair, Railfence, Rot13, Vigenere
-
+from pycipher import Atbash, Caesar, Railfence, Rot13, Vigenere,Affine
 
 app = Flask(__name__, static_folder='assets', static_url_path='/assets')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cryptolab.db'
@@ -46,8 +45,6 @@ def signin():
     # Render the sign-in page along with the form
     return render_template('signin.html', form=form)
 
-from flask import session
-
 @app.route('/logout')
 def logout():
     # Clear the user's session data
@@ -55,12 +52,29 @@ def logout():
     # Redirect the user to the sign-in page
     return redirect(url_for('signin'))
 
-
 @app.route('/dashboard')
 def dashboard():
     # This is just a placeholder for the dashboard route
     return render_template('index.html')
 
+@app.route('/caesar')
+def caesar():
+    return render_template('ceaser.html')
+
+@app.route('/RailFence')
+def rail_fence():
+    # This is just a placeholder for the Rail Fence cipher route
+    return render_template('RailFence.html')
+
+@app.route('/Playfair')
+def playfair():
+    # This is just a placeholder for the Playfair cipher route
+    return render_template('Playfair.html')
+
+@app.route('/Vigenere')
+def vigenere():
+    # This is just a placeholder for the Vigenere cipher route
+    return render_template('Vigenere.html')
 
 @app.route('/blogs')
 def blogs():
@@ -103,6 +117,7 @@ def signup():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    result = ""  # Initialize result with an empty string
     if request.method == 'POST':
         text = request.form.get('text', '')
         method = request.form.get('method', '')
@@ -112,111 +127,26 @@ def index():
         # Check if all required fields are filled
         if not text or not method:
             return "Please fill in all required fields: Text and Method"
-
-        if method in ['caesar', 'affine', 'vigenere']:
-            if not key and not shift:
-                return "Please provide either a Key or a Shift value for the chosen cipher method"
-
-        if method == 'caesar':
-            if not shift:
-                return "Please provide a Shift value for the Caesar cipher"
-            try:
-                shift = int(shift)
-            except ValueError:
-                return "Invalid Shift value. Shift must be an integer."
-
-        if method == 'vigenere':
-            if not key:
-                return "Please provide a Key for the Vigenere cipher"
-            if not key.isalpha():
-                return "Invalid Key for Vigenere cipher. Key must contain only alphabetic characters."
-
-        if method == 'affine':
-            if not key:
-                return "Please provide a Key for the Affine cipher"
-            try:
-                a, b = map(int, key.split(','))
-            except ValueError:
-                return "Invalid Key format for Affine cipher. Key must be in the format 'a,b' where a and b are integers."
-
-        if method == 'beaufort':
-            if not key:
-                return "Please provide a Key for the Beaufort cipher"
-            if not key.isalpha():
-                return "Invalid Key for Beaufort cipher. Key must contain only alphabetic characters."
-
-        # Perform the encryption/decryption based on the selected method
-        if method == 'caesar':
-            if not shift:
-                return "Please provide a Shift value for the Caesar cipher"
-            try:
-                shift = int(shift)
-            except ValueError:
-                return "Invalid Shift value. Shift must be an integer."
+            
         elif method == 'atbash':
             if request.form['action'] == 'encrypt':
-                result = Atbash().encipher(text)
+                words = text.split()  # Split the input text into individual words
+                enciphered_words = [Atbash().encipher(word) for word in words]  # Encipher each word separately
+                result = ' '.join(enciphered_words)  # Join the enciphered words with a space
             else:
-                result = Atbash().decipher(text)
-        # Other methods...
-        elif method == 'affine':
-            try:
-                a, b = map(int, key.split(','))
-            except ValueError:
-                return "Invalid key format for Affine cipher. Key must be in the format 'a,b' where a and b are integers."
-            if request.form['action'] == 'encrypt':
-                result = Affine(a, b).encipher(text)
-            else:
-                result = Affine(a, b).decipher(text)
-                
-
-        # Continuing from the previous code snippet...
-
-        elif method == 'autokey':
-            if not key:
-                return "Please provide a Key for the Autokey cipher. Example key: 'KEY'"
-            if not key.isalpha():
-                return "Invalid Key for Autokey cipher. Key must contain only alphabetic characters."
-            if request.form['action'] == 'encrypt':
-                result = ' '.join(Autokey(key).encipher(text).split())
-            else:
-                result = ' '.join(Autokey(key).decipher(text).split())
-
-        elif method == 'polybius_square':
-            if request.form['action'] == 'encrypt':
-                if len(key) != 25 and len(key) != 36:
-                    return "Invalid key for Polybius Square cipher. Key must have a length of 25 (for a 5x5 square) or 36 (for a 6x6 square)."
-                result = ' '.join(PolybiusSquare(key).encipher(text).split())
-            else:
-                if len(key) != 25 and len(key) != 36:
-                    return "Invalid key for Polybius Square cipher. Key must have a length of 25 (for a 5x5 square) or 36 (for a 6x6 square)."
-                result = ' '.join(PolybiusSquare(key).decipher(text).split())
-
-        elif method == 'playfair':
-            if not key:
-                return "Please provide a Key for the Playfair cipher. Example key: 'KEYWORD'"
-            if not key.isalpha():
-                return "Invalid Key for Playfair cipher. Key must contain only alphabetic characters."
-            if request.form['action'] == 'encrypt':
-                result = ' '.join(Playfair(key).encipher(text).split())
-            else:
-                result = ' '.join(Playfair(key).decipher(text).split())
-
-        elif method == 'rail_fence':
-            if not key:
-                return "Please provide a Key for the Rail Fence cipher. Example key: '3'"
-            if not key.isdigit():
-                return "Invalid Key for Rail Fence cipher. Key must be a positive integer."
-            if request.form['action'] == 'encrypt':
-                result = ' '.join(Railfence(int(key)).encipher(text).split())
-            else:
-                result = ' '.join(Railfence(int(key)).decipher(text).split())
+                words = text.split()  # Split the input text into individual words
+                deciphered_words = [Atbash().decipher(word) for word in words]  # Decipher each word separately
+                result = ' '.join(deciphered_words)  # Join the deciphered words with a space
 
         elif method == 'rot13':
             if request.form['action'] == 'encrypt':
-                result = ' '.join(Rot13().encipher(text).split())
+                words = text.split()  # Split the input text into individual words
+                enciphered_words = [Rot13().encipher(word) for word in words]  # Encipher each word separately
+                result = ' '.join(enciphered_words)  # Join the enciphered words with a space
             else:
-                result = ' '.join(Rot13().decipher(text).split())
+                words = text.split()  # Split the input text into individual words
+                deciphered_words = [Rot13().decipher(word) for word in words]  # Decipher each word separately
+                result = ' '.join(deciphered_words)  # Join the deciphered words with a space
 
         elif method == 'vigenere':
             if not key:
@@ -224,22 +154,66 @@ def index():
             if not key.isalpha():
                 return "Invalid Key for Vigenère cipher. Key must contain only alphabetic characters."
             if request.form['action'] == 'encrypt':
-                result = ' '.join(Vigenere(key).encipher(text).split())
+                words = text.split()  # Split the input text into individual words
+                enciphered_words = [Vigenere(key).encipher(word) for word in words]  # Encipher each word separately
+                result = ' '.join(enciphered_words)  # Join the enciphered words with a space
             else:
-                result = ' '.join(Vigenere(key).decipher(text).split())
+                words = text.split()  # Split the input text into individual words
+                deciphered_words = [Vigenere(key).decipher(word) for word in words]  # Decipher each word separately
+                result = ' '.join(deciphered_words)  # Join the deciphered words with a space
 
-        # If the method is not recognized
+        elif method == 'caesar':
+            if not shift:
+                return "Please provide a Shift value for the Caesar cipher"
+            try:
+                shift = int(shift)
+            except ValueError:
+                return "Invalid Shift value. Shift must be an integer."
+            if request.form['action'] == 'encrypt':
+                words = text.split()  # Split the input text into individual words
+                enciphered_words = [Caesar(shift).encipher(word) for word in words]  # Encipher each word separately
+                result = ' '.join(enciphered_words)  # Join the enciphered words with a space
+            else:
+                words = text.split()  # Split the input text into individual words
+                deciphered_words = [Caesar(shift).decipher(word) for word in words]  # Decipher each word separately
+                result = ' '.join(deciphered_words)  # Join the deciphered words with a space
+                
+        elif method == 'affine':
+            if not key:
+                return "Please provide a Key for the Affine cipher"
+            try:
+                a, b = map(int, key.split(','))
+            except ValueError:
+                return "Invalid Key format for Affine cipher. Key must be in the format '1,2' where 1 and 2 are integers."
+            if request.form['action'] == 'encrypt':
+                words = text.split()  # Split the input text into individual words
+                enciphered_words = [Affine(a, b).encipher(word) for word in words]  # Encipher each word separately
+                result = ' '.join(enciphered_words)  # Join the enciphered words with a space
+            else:
+                words = text.split()  # Split the input text into individual words
+                deciphered_words = [Affine(a, b).decipher(word) for word in words]  # Decipher each word separately
+                result = ' '.join(deciphered_words)  # Join the deciphered words with a space
+
+        elif method == 'rail_fence':
+            if not key:
+                return "Please provide a Key for the Rail Fence cipher. Example key: '3'"
+            if not key.isdigit():
+                return "Invalid Key for Rail Fence cipher. Key must be a positive integer less than 8 example '3'."
+
+            if request.form['action'] == 'encrypt':
+                words = text.split()  # Split the input text into individual words
+                enciphered_words = [Railfence(int(key)).encipher(word) for word in words]  # Encipher each word separately
+                result = ' '.join(enciphered_words)  # Join the enciphered words with a space
+            else:
+                words = text.split()  # Split the input text into individual words
+                deciphered_words = [Railfence(int(key)).decipher(word) for word in words]  # Decipher each word separately
+                result = ' '.join(deciphered_words)  # Join the deciphered words with a space
+
         else:
             result = "Invalid method"
-
         return result
-
-
     return render_template('index.html')
-
-
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
-    
